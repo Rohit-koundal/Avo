be05def9bea7041854554d36d447c81416084cb8
Rohit-koundal <rkshekhavat@gmail.com>
2024-03-21 21:26:52 +0530

    latest one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <><div>.</div></>
+    <><h1>Welcome</h1></>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
